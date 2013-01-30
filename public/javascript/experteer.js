080d1f2f1f6740086fe3a13ef8c58175de2b3b91
Julian Duque <julianduquej@gmail.com>
2013-01-30 16:51:38 -0500

    fix combos, add form editables in profile

--- public/javascript/experteer.js
+++ public/javascript/experteer.js
@@ -20,9 +20,25 @@ $(document).ready(function() {
     buttonImage: "/images/btn_datepicker.png",
     buttonImageOnly: true
   });
-  return $('.tags').tagit({
+  $('.tags').tagit({
     availableTags: ["Option 1", "Option 2", "Option 3", "Option 4"],
     create: function(event, ui) {},
     onTagAdded: function(event, tag) {}
+  });
+  $(".combobox").combobox({});
+  return $("#add_support").click(function(e) {
+    var label, option, value;
+    e.preventDefault();
+    value = $('#area_support').val();
+    label = $('#area_support option:selected').text();
+    if (value !== '' && $("input[name='area_support'][value='" + value + "']").length === 0 && $("input[name='area_support']").length < 6) {
+      option = "<p class='tagit-choice'><span class='tagit-label'>" + label + "</span><a class='close'><span class='text-icon'>×</span><span class='ui-icon ui-icon-close'></span></a><input type='hidden' name='area_support' value='" + value + "' /></p>";
+      $('#areas').append(option);
+      $(this).parent().val("");
+      return $("p.tagit-choice .close").click(function(e) {
+        e.preventDefault();
+        return $(this).parent().remove();
+      });
+    }
   });
 });
--- views/coffee/experteer.coffee
+++ views/coffee/experteer.coffee
@@ -23,3 +23,21 @@ $(document).ready ->
     availableTags: ["Option 1","Option 2","Option 3","Option 4"]
     create: (event, ui) ->
     onTagAdded: (event, tag) ->
+
+  $(".combobox").combobox {}
+
+  $("#add_support").click (e) ->
+    e.preventDefault()
+
+    value = $('#area_support').val()
+    label = $('#area_support option:selected').text()
+
+    if value != '' && $("input[name='area_support'][value='#{value}']").length == 0 && $("input[name='area_support']").length < 6
+      option = "<p class='tagit-choice'><span class='tagit-label'>#{label}</span><a class='close'><span class='text-icon'>×</span><span class='ui-icon ui-icon-close'></span></a><input type='hidden' name='area_support' value='#{value}' /></p>"
+
+      $('#areas').append(option);
+      $(this).parent().val("")
+
+      $("p.tagit-choice .close").click (e) ->
+        e.preventDefault()
+        $(this).parent().remove()
