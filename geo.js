2588dcd4c3b91ce7fbaa0d91d92c866c12de5009
Thomas Blobaum <tblobaum@gmail.com>
2013-05-30 22:05:52 -0500

    grab lat and lng from google maps

---
+++ geo.js
@@ -0,0 +1,38 @@
+
+var request = require('request')
+  , cacheGeoCode = {}
+
+function setupGeo (opts) {
+  opts = opts || {}
+  opts.timeout = opts.timeout || process.env.yahooTimeout || 2500
+
+  return function geo (keywords, callback) {
+    if (cacheGeoCode[keywords]) {
+      return callback(null, cacheGeoCode[keywords])
+    }
+    var params = { 
+      timeout : opts.timeout, 
+      url : 'http://maps.googleapis.com/maps/api/geocode/json?'
+        +'address='+keywords
+        +'&sensor=false'
+    }
+    request(params, function (error, response, body) {
+      if (!error) {
+        try {
+          var data = JSON.parse(body)
+        } catch (err) {
+          console.error(err, body)
+          return callback(err)
+        } finally {
+          data.statusCode = response.statusCode
+          cacheGeoCode[keywords] = data
+          return callback(null, data)
+        }
+      } else {
+        return callback(error)
+      }
+    })
+  }
+}
+
+module.exports = setupGeo
