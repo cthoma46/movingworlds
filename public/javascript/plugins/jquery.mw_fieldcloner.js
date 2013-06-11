e83fdecb801952ddcc51216e54c16470722e3a48
Gilad Gray <giladgray@gmail.com>
2013-06-10 22:33:55 -0700

    jquery.mw_fieldcloner rewrite
    add button is now part of template, no more minus button, cleaner code.

--- public/javascript/plugins/jquery.mw_fieldcloner.js
+++ public/javascript/plugins/jquery.mw_fieldcloner.js
@@ -4,99 +4,54 @@
 
 		init: function(options) {
 			
-			settings = $.extend( {
-					childType: 'fieldset'
-				,	deleteBtnClass: '.delete_btn'
-				, navMarkup : '<nav><ul><li><a href="#" class="add_btn">+</a></li><li><a href="#" class="minus_btn">-</a></li></ul></nav>'
+			settings = $.extend({
+				childType: '.pure-u-1',
+				deleteBtnClass: '.delete_btn',
+				addBtnClass: '.add_btn'
 			}, options);
 
-     	return this.each(function(){
-
-         var $this = $(this),
-             data = $this.data('mwFieldCloner');
-				
-        if ( ! data ) {
-
+	     	return this.each(function() {
+		        var $this = $(this),
+		            data = $this.data('mwFieldCloner');
+						
+		        if ( !data ) {
 					$this.data('mwFieldCloner', {
-							template : $( "#"+$this.data('template') ).html()
-						, total : $this.children( settings.childType ).length
-						, $nav : $(settings.navMarkup).appendTo( $this )
+						template : $( "#"+$this.data('template') ).html(),
+						total : $this.children( settings.childType ).length
 					});
-
 				}
 
 				// enable all delete buttons
 				$(settings.deleteBtnClass).click( function(e){
 					e.preventDefault();
+					// walk up the tree to find first child element
 					removeTemplate( $(this).parents(settings.childType).first() );
 				})
 
-
-
-				$this.data('mwFieldCloner').$nav.find('.minus_btn').click( function(e){
+				$this.find(addBtnClass).click( function(e){
 					e.preventDefault();
-					removeTemplate();
-				})
-
-				$this.data('mwFieldCloner').$nav.find('.add_btn').click( function(e){
-					e.preventDefault();
+					// just add a new template to the element
 					addTemplate();
 				});
 
-
-
-				// addTemplate( $this.data('mwFieldCloner').template );
-
-				// console.log( data.settings )
-
 				// FUNCTIONS
-
 				function addTemplate() {
-
-					console.log('addTemplate'); 
-						
-					var $temp = $( $this.data('mwFieldCloner').template ).insertBefore( $this.data('mwFieldCloner').$nav );
-
+					var $temp = $( $this.data('mwFieldCloner').template ).insertAfter( $this.find(settings.childType + ":last") );
 					$temp.find(settings.deleteBtnClass).removeClass('hidden').click( function(e){
 						e.preventDefault();
 						removeTemplate( $temp )
 					})
-
 					$this.data('mwFieldCloner').total++;
-					configureNav();
 					configureFields();
-
 				}
 
-				function	removeTemplate( item ) {
+				function removeTemplate( item ) {
 					console.log('removeTemplate');
 					if(!item) {
 						item = $this.find( settings.childType ).last();
 					}
-
-							console.log(item); 
-						
-				
-					if ($this.data('mwFieldCloner').total > 1) {
-						$(item).remove()
-						$this.data('mwFieldCloner').total--;
-					}
-					configureNav();
+					$(item).remove()
 					configureFields();
-				}
-
-
-				function configureNav() {
-					console.log('configureNav');
-					var $minusBtn = $this.data('mwFieldCloner').$nav.find('.minus_btn');
-
-					if($this.data('mwFieldCloner').total == 1 && !$minusBtn.hasClass('disabled')){
-						$minusBtn.addClass('disabled')
-					}
-					else if($this.data('mwFieldCloner').total > 1 && $minusBtn.hasClass('disabled')) {
-						$minusBtn.removeClass('disabled')
-					}
-
 				}
 
 				function configureFields() {
@@ -106,54 +61,37 @@
 						console.log('count',i, this);
 
 						$(this).find('input, select, textarea').each(function(j){
-
-
 							var $this = $(this);
 
+							// replace that placeholder with the latest index
 							if( $this.attr('name') ) {
 								var newName = $this.attr('name').replace(/\[##\]/, "["+i+"]");
 							}
-							// console.log($this, newName);
-
+							// now go udpate other relevant attributes
 							if( newName ){
-												
 								$this.attr('name', newName);
 								$this.attr('id', newName);
 								$this.attr('tabIndex', $this.attr('tabindex')*100 ); 
 
 								$this.siblings('label').first().attr('for', newName)
 							}
-
 						});
-
 					});
 
-
-
 					$(".tags").tagit();
-
-
 					$( ".datepicker input" ).datepicker({
-						changeMonth: true
-						,	changeYear: true
-						,	buttonImage: "/images/btn_datepicker.png"
-						,	showOn: "both"
-						,	buttonImageOnly: true
-						}
-					);
-
+						changeMonth: true,
+						changeYear: true,
+						buttonImage: "/images/btn_datepicker.png",
+						showOn: "both",
+						buttonImageOnly: true
+					});
 				}
-
-			
 			});
-
 		}
-
-	
 	};
 
 	$.fn.MwFieldCloner = function(method) {
-
 	    if (methods[method]) {
 	        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 	    } else if (typeof method === 'object' || !method) {
